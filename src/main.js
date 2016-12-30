d3b96241fcaacd4d0cec7e695621edabd01ebacf
the-fonz <murstor@gmail.com>
2016-12-30 16:30:26 +0100

    Refactored Game class into its own file

--- src/main.js
+++ src/main.js
@@ -2,13 +2,9 @@
  * Collection of all entry points. They load by identifying the body's id tag
  */
 
-import {Terrain} from './game-engine/terrain';
-import {Engine} from './game-engine/engine';
-import {HeightmapView} from './render/view2d';
-import {ThreeDeeView} from './render/view3d';
+import {Game} from './game';
 import {promiseGet} from './utils/http';
 import {l} from './utils/logging';
-import {KeyMap} from './utils/input';
 import {BASICCONFIG} from './config';
 
 // Entry point for terrain load example
@@ -57,67 +53,4 @@ if (document.body.id === "freefly-example") {
     let pgmodels = values.slice(1);
     let g = new Game(terrainmodel, pgmodels, config);
   });
-}
-
-
-
-class Game {
-  constructor(terrainmodel, pgmodels, config){
-    // Load terrain, engine, view (in that order)
-    var km = new KeyMap();
-    l("Building mountains...");
-    var t = new Terrain(terrainmodel.xcgame.heightmap,
-      terrainmodel.xcgame.hscale,
-      // Vertical scale of terrain mesh
-      terrainmodel.xcgame.vscale,
-      // Heightmap multiplier, excludes vscale
-      terrainmodel.xcgame.heightmapvscale);
-    l("Retrieving vectors...");
-    var e = new Engine(t, config.Engine);
-    l("Generating triangles...");
-    var v = new ThreeDeeView(e, terrainmodel, pgmodels, config.ThreeDeeView);
-
-    l("Setting time interval...");
-    var blur = false;
-    // requestAnimationFrame only runs when tab is active
-    // Handle blur/focus event to stop simulation
-    // when window is hidden while tab is active
-    window.addEventListener('blur', (ev) => {
-      blur=true;
-      l("window blurred, stopping simulation");
-    });
-    window.addEventListener('focus', (ev) => {
-      blur=false;
-      l("window focused, resuming simulation");
-    });
-
-    var time = 0;
-    function renderloop(timestamp) {
-      if (time===0) time = timestamp;
-
-      let dt = (timestamp-time)/1000;
-
-      if (blur === false) {
-        if (pgmodels) {
-          e.paragliders[0].input(dt, km);
-          v.updatePg();
-          v.updateShadow(e.paragliders[0]);
-        }
-        // Switch camera
-        if (km.get(" ")) {
-          km.reset(" ");
-          v.nextCam();
-        }
-        v.cam(km, dt, e.paragliders[0]);
-        e.update(dt);
-        // l("animating");
-        v.render();
-      }
-
-      requestAnimationFrame(renderloop);
-      time = timestamp;
-    }
-
-    requestAnimationFrame(renderloop);
-  }
 }
